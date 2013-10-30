d24cdab804f21ca84c91f8a29bc7c20bdbb1033a
Toshihiko Ohba <t-ohba@t-ohba-virtual-machine.(none)>
2013-10-30 10:41:13 +0900

    add load_balancer_t-ohba.rb

--- load_balancer_t-ohba.rb
+++ load_balancer_t-ohba.rb
@@ -4,9 +4,12 @@ require "logger"
 class MyLoadBalancer < Controller
  SERVER_BASE_ADDRESS = 250
  SERVER_COUNT = 4
+ HARD_TIMEOUT = 1
 
  def start
   @fdb = {}
+  @server_list = {}
+  @server_ip = []
   @log = Logger.new('test.log', 7)
   @log.level = Logger::DEBUG
  end
@@ -24,7 +27,7 @@ class MyLoadBalancer < Controller
   elsif message.arp_reply?
    handle_arp_reply(dpid, message)
   elsif message.ipv4?
-   handle_ipv4(dpid, message)
+   handle_ipv4(dpid, message, port)
   end
  end
 
@@ -49,39 +52,68 @@ class MyLoadBalancer < Controller
  def handle_arp_request(dpid, message)
   @log.debug("arp_request eth_type " + message.eth_type.to_s + " in_port : " + message.in_port.to_s + " sha : " + message.arp_sha.to_s + " spa : " + message.arp_spa.to_s + " tha : " + message.arp_tha.to_s + " tpa : " + message.arp_tpa.to_s)
 
-  send_packet(dpid, message, OFPP_FLOOD)
+  send_packet(dpid, message, SendOutPort.new(OFPP_FLOOD))
  end
 
  def handle_arp_reply(dpid, message)
   @log.debug("arp_reply eth_type " + message.eth_type.to_s + " in_port : " + message.in_port.to_s + " sha : " + message.arp_sha.to_s + " spa : " + message.arp_spa.to_s + " tha : " + message.arp_tha.to_s + " tpa : " + message.arp_tpa.to_s)
 
-  send_packet(dpid, message, OFPP_FLOOD)
+  register_server(message)
+  send_packet(dpid, message, SendOutPort.new(OFPP_FLOOD))
  end
 
- def handle_ipv4(dpid, message)
-  @log.debug("eth_type " + message.eth_type.to_s + " in_port : " + message.in_port.to_s + " nwsa : " + message.ipv4_saddr.to_s + " nwda : " + message.ipv4_daddr.to_s)
-
-  if port
-    add_flow(dpid, message, port)
-    send_packet(dpid, message, port)
-  else
-    send_packet(dpid, message, OFPP_FLOOD)
+ def register_server(message)
+  address = message.arp_spa.to_s.split(".")[3].to_i
+  if address >= SERVER_BASE_ADDRESS
+    @server_list[message.arp_spa.to_s] = message.arp_sha
+    @server_ip << message.arp_spa.to_s
+    @log.debug("register : src_ip " + message.arp_spa.to_s + " src_mac " + message.arp_sha.to_s)
   end
  end
 
- def add_flow(dpid, message, port)
+ def handle_ipv4(dpid, message, port)
+  @log.debug("eth_type " + message.eth_type.to_s + " in_port : " + message.in_port.to_s + " nwsa : " + message.ipv4_saddr.to_s + " nwda : " + message.ipv4_daddr.to_s)
+
+  address = message.ipv4_daddr.to_s.split(".")[3].to_i
+  if address >= SERVER_BASE_ADDRESS
+    to_server = true
+  else
+    to_server = false
+  end
+
+  if to_server
+    address = @server_ip[rand(SERVER_COUNT+1)]
+    action = [
+      SetIpDstAddr.new(address),
+      SetEthDstAddr.new(@server_list[address]),
+      SendOutPort.new(@fdb[@server_list[address]])
+    ]
+    add_flow(dpid, message, action)
+    send_packet(dpid, message, action)
+  else
+    if port
+      add_flow(dpid, message, SendOutPort.new(port))
+      send_packet(dpid, message, SendOutPort.new(port))
+    else
+      send_packet(dpid, message, SendOutPort.new(OFPP_FLOOD))
+    end
+  end 
+ end
+
+ def add_flow(dpid, message, action)
   send_flow_mod_add(
       dpid,
-      :match => ExactMatch.from(message),
-      :actions => Trema::SendOutPort.new(port)
+      :hard_timeout => HARD_TIMEOUT,
+      :match => Match.from(message),
+      :actions => action
      )
  end
 
- def send_packet(dpid, message, port)
+ def send_packet(dpid, message, action)
   send_packet_out(
      dpid,
      :packet_in => message,
-     :actions => Trema::SendOutPort.new(port)
+     :actions => action
     )
  end
 end
