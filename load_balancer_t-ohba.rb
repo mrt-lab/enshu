a85cbcfc515c5710bcc7188b65ecb85bca8547a3
Toshihiko Ohba <t-ohba@t-ohba-virtual-machine.(none)>
2013-10-30 13:41:48 +0900

    add load_balancer_t-ohba.rb

--- load_balancer_t-ohba.rb
+++ load_balancer_t-ohba.rb
@@ -2,8 +2,8 @@ require "pio"
 require "logger"
 
 class MyLoadBalancer < Controller
- SERVER_BASE_ADDRESS = 250
- SERVER_COUNT = 4
+ SERVER_BASE_ADDRESS = 128
+ SERVER_COUNT = 126
  HARD_TIMEOUT = 1
 
  def start
@@ -25,7 +25,7 @@ class MyLoadBalancer < Controller
   if message.arp_request?
    handle_arp_request(dpid, message)
   elsif message.arp_reply?
-   handle_arp_reply(dpid, message)
+   handle_arp_reply(dpid, message, port)
   elsif message.ipv4?
    handle_ipv4(dpid, message, port)
   end
@@ -52,22 +52,26 @@ class MyLoadBalancer < Controller
  def handle_arp_request(dpid, message)
   @log.debug("arp_request eth_type " + message.eth_type.to_s + " in_port : " + message.in_port.to_s + " sha : " + message.arp_sha.to_s + " spa : " + message.arp_spa.to_s + " tha : " + message.arp_tha.to_s + " tpa : " + message.arp_tpa.to_s)
 
-  send_packet(dpid, message, SendOutPort.new(OFPP_FLOOD))
+  action = [SendOutPort.new(OFPP_FLOOD)]
+  add_flow(dpid, 0, message, action)
+  send_packet(dpid, message, action)
  end
 
- def handle_arp_reply(dpid, message)
+ def handle_arp_reply(dpid, message, port)
   @log.debug("arp_reply eth_type " + message.eth_type.to_s + " in_port : " + message.in_port.to_s + " sha : " + message.arp_sha.to_s + " spa : " + message.arp_spa.to_s + " tha : " + message.arp_tha.to_s + " tpa : " + message.arp_tpa.to_s)
 
   register_server(message)
-  send_packet(dpid, message, SendOutPort.new(OFPP_FLOOD))
+  action = [SendOutPort.new(OFPP_FLOOD)]
+  add_flow(dpid, 0, message, action)
+  send_packet(dpid, message, action)
  end
 
  def register_server(message)
   address = message.arp_spa.to_s.split(".")[3].to_i
-  if address >= SERVER_BASE_ADDRESS
+  if ( address >= SERVER_BASE_ADDRESS ) && ( @server_ip.index(message.arp_spa.to_s).nil? )
     @server_list[message.arp_spa.to_s] = message.arp_sha
     @server_ip << message.arp_spa.to_s
-    @log.debug("register : src_ip " + message.arp_spa.to_s + " src_mac " + message.arp_sha.to_s)
+    @log.debug("register : src_ip " + message.arp_spa.to_s + " src_mac " + message.arp_sha.to_s + " length = " + @server_ip.length.to_s)
   end
  end
 
@@ -82,17 +86,17 @@ class MyLoadBalancer < Controller
   end
 
   if to_server
-    address = @server_ip[rand(SERVER_COUNT+1)]
+    address = @server_ip[rand(@server_ip.length)]
     action = [
       SetIpDstAddr.new(address),
       SetEthDstAddr.new(@server_list[address]),
       SendOutPort.new(@fdb[@server_list[address]])
     ]
-    add_flow(dpid, message, action)
+    add_flow(dpid, 1, message, action)
     send_packet(dpid, message, action)
   else
     if port
-      add_flow(dpid, message, SendOutPort.new(port))
+      add_flow(dpid, 1, message, SendOutPort.new(port))
       send_packet(dpid, message, SendOutPort.new(port))
     else
       send_packet(dpid, message, SendOutPort.new(OFPP_FLOOD))
@@ -100,10 +104,10 @@ class MyLoadBalancer < Controller
   end 
  end
 
- def add_flow(dpid, message, action)
+ def add_flow(dpid, time, message, action)
   send_flow_mod_add(
       dpid,
-      :hard_timeout => HARD_TIMEOUT,
+      :hard_timeout => time,
       :match => Match.from(message),
       :actions => action
      )
